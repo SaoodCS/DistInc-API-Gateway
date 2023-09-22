c1b01b95126b8aa7e6d1c390baf1864e0e933384
SaoodCS <saood-aslam@outlook.com>
2023-09-22 12:28:19 +0100

    correct url for deployed apps

--- functions/src/utils/CorsPolicy.ts
+++ functions/src/utils/CorsPolicy.ts
@@ -2,8 +2,8 @@ export const authorisedDomains = [
    'http://localhost:5173',
    'https://localhost:5173',
    'http://localhost:5000',
-   'https://distinc-9ad9d.web.app/',
-   'https://distinc-9ad9d.firebaseapp.com/',
-   'https://distinc-dev.web.app/',
-   'https://distinc-dev.firebaseapp.com/',
+   'https://distinc-9ad9d.web.app',
+   'https://distinc-9ad9d.firebaseapp.com',
+   'https://distinc-dev.web.app',
+   'https://distinc-dev.firebaseapp.com',
 ];
