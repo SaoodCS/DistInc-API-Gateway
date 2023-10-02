084fa4ccfb5cdd8bd5c74539d4c72e2535e4c273
SaoodCS <saood-aslam@outlook.com>
2023-10-02 10:27:19 +0100

    add new delete user microservice

--- functions/src/utils/Microservices.ts
+++ functions/src/utils/Microservices.ts
@@ -7,6 +7,7 @@ export interface IMicroservices {
 const microservices: IMicroservices[] = [
    { service: 'testServiceEndpoint', url: process.env.TEST_SERVICE_ENDPOINT!, los: 0 },
    { service: 'registerUser', url: process.env.REGISTER_USER_SERVICE_ENDPOINT!, los: 0 },
+   { service: 'deleteUser', url: process.env.DELETE_USER_SERVICE_ENDPOINT!, los: 1 },
 ];
 
 export default microservices;
