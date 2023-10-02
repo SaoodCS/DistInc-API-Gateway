b9f4aa69b29ceb6e30bed601631bd9ec811f6351
SaoodCS <saood-aslam@outlook.com>
2023-10-02 10:51:23 +0100

    adding the resetUser microservice

--- functions/src/utils/Microservices.ts
+++ functions/src/utils/Microservices.ts
@@ -8,6 +8,7 @@ const microservices: IMicroservices[] = [
    { service: 'testServiceEndpoint', url: process.env.TEST_SERVICE_ENDPOINT!, los: 0 },
    { service: 'registerUser', url: process.env.REGISTER_USER_SERVICE_ENDPOINT!, los: 0 },
    { service: 'deleteUser', url: process.env.DELETE_USER_SERVICE_ENDPOINT!, los: 1 },
+   { service: 'resetUser', url: process.env.RESET_USER_SERVICE_ENDPOINT!, los: 1 },
 ];
 
 export default microservices;
