02c822816c63cb8c18da2862be2919365a565cb6
SaoodCS <saood-aslam@outlook.com>
2023-11-26 15:06:07 +0000

    adding localnet to cors policy

--- functions/src/utils/CorsPolicy.ts
+++ functions/src/utils/CorsPolicy.ts
@@ -2,6 +2,7 @@ export const authorisedDomains = [
    'http://localhost:5173',
    'https://localhost:5173',
    'http://localhost:5000',
+   'https://192.168.1.80:5173',
    'https://distinc-9ad9d.web.app',
    'https://distinc-9ad9d.firebaseapp.com',
    'https://distinc-dev.web.app',
